f1534e4d13a3c1676c939a86f2baf07151ae5436
Julie Rindone <julie.rindone@gmail.com>
2021-01-28 16:43:41 -0600

    Ch 13 examples

---
+++ CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        private static List<string> Events = new List<string>();            //b/c List is static, we can access it from ALL our methods
+
+        //get: /controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+    // get: /Events/Add
+    [HttpGet]
+    public IActionResult Add()     //we are adding the form that we make in View.
+        {
+            return View();
+        }
+
+      //Post: /Events/Add
+    [HttpPost]
+    [Route("/Events/Add/")]
+    public IActionResult NewEvent(string name)       //handling our post request made in Add()
+        {
+            Events.Add(name);
+
+            return Redirect("/Events");
+        }
+
+    }
+}
