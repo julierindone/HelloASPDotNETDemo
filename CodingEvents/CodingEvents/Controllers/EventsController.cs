e7c12da2f238fa19ac605b82cbf9c3fed36cc8de
Julie Rindone <julie.rindone@gmail.com>
2021-01-29 09:39:19 -0600

    Ch13Exercise complte

--- CodingEvents/CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -8,33 +8,35 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        private static List<string> Events = new List<string>();            //b/c List is static, we can access it from ALL our methods
-
-        //get: /controller>/
-        [HttpGet]
+        private static Dictionary<string, string> Events = new Dictionary<string, string>();
         public IActionResult Index()
-        {
+        {            
+/*            Events.Add("Code With Pride");
+            Events.Add("Apple WWDC");
+            Events.Add("Strange Loop");*/
             ViewBag.events = Events;
 
             return View();
         }
 
-    // get: /Events/Add
-    [HttpGet]
-    public IActionResult Add()     //we are adding the form that we make in View.
+        // get: /Events/Add
+        [HttpGet]
+        public IActionResult Add()     //we are adding the form that we make in View.
         {
             return View();
         }
 
-      //Post: /Events/Add
-    [HttpPost]
-    [Route("/Events/Add/")]
-    public IActionResult NewEvent(string name)       //handling our post request made in Add()
+        //Post: /Events/Add
+        [HttpPost]
+        [Route("/Events/Add/")]
+        public IActionResult NewEvent(string name, string description)       //handling our post request made in Add()
         {
-            Events.Add(name);
+            Events.Add(name, description);
 
             return Redirect("/Events");
         }
 
     }
 }
+
+
